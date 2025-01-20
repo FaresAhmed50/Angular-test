d3becff3a4275ef5b670bd41b42d8cd99c2ea910
fares <fares.ahmed15967@aiet.edu.eg>
2025-01-20 02:54:15 +0200

    finsing Star & portfolio componetn

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {StarComponent} from "../star/star.component";
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [
+    StarComponent,
+    NgClass
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+
+  images: string[] = [
+    "./poert1.png",
+    "./port2.png",
+    "./port3.png",
+    "./poert1.png",
+    "./port2.png",
+    "./port3.png"
+  ]
 
 }
