3dbd7b6794de5ed9d192ea914cd01e6037abb36e
fares <fares.ahmed15967@aiet.edu.eg>
2025-01-20 04:23:46 +0200

    Final changes

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
+import {NgClass} from '@angular/common';
 
+// @ts-ignore
 @Component({
   selector: 'app-star',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './star.component.html',
-  styleUrl: './star.component.css'
+  styleUrl: './star.component.css',
+  styles:[
+
+  ]
 })
-export class StarComponent {
+export class StarComponent  {
+
+  bgcolor !: string;
+  @Input() style: 'dark' | 'white' = 'white';
+  // @HostBinding('style.--bgcolor') color = this.bgcolor;
+
+
+  get iconColor(): string {
+    this.bgcolor = this.style;
+    return this.style === 'dark' ? 'text-dark' : 'text-white';
+  }
+
+
 
 }
