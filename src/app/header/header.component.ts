17ad6a2c12e40aad3199fdb575b7176b47e36b1d
fares <fares.ahmed15967@aiet.edu.eg>
2025-01-20 02:52:44 +0200

    Finishing nav & footer & home component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  selector: 'app-nav',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
 })
-export class HeaderComponent {
+export class NavComponent {
 
 }
